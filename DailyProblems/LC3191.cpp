ad2b3b5f76dcb4ee501a627126d8e32152e3b775
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-03-19 18:39:54 +0530

    solved lc daily

---
+++ DailyProblems/LC3191.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int minOperations(vector<int>& nums) {
+            int count=0;
+            int n=nums.size();
+            for(int i=0;i<n-2;i++){
+                if(nums[i]==0){
+                    count++;
+                    nums[i+1]=!nums[i+1];
+                    nums[i+2]=!nums[i+2];
+                }
+            }
+            if(nums[n-2]==0 || nums[n-1]==0){
+                return -1;
+            }
+            return count;
+        }
+    };
